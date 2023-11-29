c393807319f734d20597bc4e7589eb8225e72432
IntegerVector <andrii.ruban@outlook.com>
2023-11-29 18:16:06 +0200

    sh: add links script

---
+++ link.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -s ~/Projects/dotfiles/.config/kitty ~/.config/kitty
+ln -s ~/Projects/dotfiles/.shell ~/.shell
+ln -s ~/Projects/dotfiles/.zshrc ~/.zshrc
+ln -s ~/Projects/dotfiles/.vimrc ~/.vimrc
+
