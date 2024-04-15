7e3a4f194de4fa8413df0193f9f99e604705cbff
IntegerVector <andrii.ruban@outlook.com>
2024-04-15 16:35:11 +0300

    nvim: add link to nvim configs

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 ln -s ~/Projects/dotfiles/.config/kitty ~/.config/kitty
+ln -s ~/Projects/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/Projects/dotfiles/.shell ~/.shell
 ln -s ~/Projects/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Projects/dotfiles/.vimrc ~/.vimrc
