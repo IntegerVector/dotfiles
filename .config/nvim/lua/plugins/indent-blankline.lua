52a6d581b6b7659e5fc55b40d95749a073de4503
IntegerVector <andrii.ruban@outlook.com>
2024-04-16 12:08:21 +0300

    nvim: new indent style

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -2,8 +2,8 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   opts = {
     indent = {
-      char = '│',
-      tab_char = '│',
+      char = '',
+      tab_char = '',
     },
     scope = { enabled = false },
     exclude = {
