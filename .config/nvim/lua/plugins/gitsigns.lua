222ef6f8aef77d9f0009954ebd649745ff0059c4
IntegerVector <andrii.ruban@outlook.com>
2024-04-17 20:33:21 +0300

    nvim: change gitsig mappings

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -24,7 +24,7 @@ return {
       map('n', '<leader>gu', gs.undo_stage_hunk, 'Undo Stage Hunk')
       map('n', '<leader>gS', gs.stage_buffer, 'Stage Buffer')
       map('n', '<leader>gR', gs.reset_buffer, 'Reset Buffer')
-      map('n', '<leader>gb', function() gs.blame_line({ full = true }) end, 'Blame Line')
+      map('n', '<leader>m', function() gs.blame_line({ full = true }) end, 'Blame Line')
       map('n', '<leader>gd', gs.diffthis, 'Diff This')
       map('n', '<leader>gD', function() gs.diffthis('~') end, 'Diff This ~')
     end,
