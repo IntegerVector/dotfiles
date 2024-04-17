bc7d1afdc07869f68bdd900701ef245edbc237cd
IntegerVector <andrii.ruban@outlook.com>
2024-04-17 20:34:03 +0300

    nvim: add lsp mappings

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -47,6 +47,19 @@ return {
     }
 
     vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<Leader>c', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- vim.keymap.set('n', '<Leader>F', function()
+    --  vim.lsp.buf.format { async = true }
+    -- end, opts)
   end
 }
 
