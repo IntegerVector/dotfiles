b511b2932dcf914feb5ac5d984bdf6dad718be58
IntegerVector <andrii.ruban@outlook.com>
2024-09-20 19:39:20 +0300

    nvim: telescope-file-browser added for quick file switching

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ return {
   config = function()
     local actions = require "telescope.actions"
     local builtin = require("telescope.builtin")
+    local fb_actions = require("telescope._extensions.file_browser.actions")
 
     require("telescope").setup {
       extensions = {
@@ -20,9 +21,59 @@ return {
           override_generic_sorter = true,
           override_file_sorter = true,
           case_mode = "smart_case",
-        }
+        },
+        file_browser = {
+          initial_mode = "normal",
+          theme="ivy",
+          hijack_netrw = false,
+          select_buffer = true,
+          path = "%:p:h",
+          display_stat = { date = false, size = true, mode = false },
+          mappings = {
+            ["i"] = {
+              ["<C-a>"] = fb_actions.create,
+              ["<S-CR>"] = fb_actions.create_from_prompt,
+              ["<A-r>"] = fb_actions.rename,
+              ["<A-m>"] = fb_actions.move,
+              ["<A-c>"] = fb_actions.copy,
+              ["<A-d>"] = fb_actions.remove,
+              ["<C-g>"] = fb_actions.goto_parent_dir,
+              ["<C-e>"] = fb_actions.goto_home_dir,
+              ["<C-w>"] = fb_actions.goto_cwd,
+              ["<C-t>"] = fb_actions.change_cwd,
+              ["<C-f>"] = fb_actions.toggle_browser,
+              ["<C-h>"] = fb_actions.toggle_hidden,
+              ["<C-s>"] = fb_actions.toggle_all,
+            },
+            ["n"] = {
+              ["a"] = fb_actions.create,
+              ["r"] = fb_actions.rename,
+              ["m"] = fb_actions.move,
+              ["c"] = fb_actions.copy,
+              ["d"] = fb_actions.remove,
+              ["g"] = fb_actions.goto_parent_dir,
+              ["e"] = fb_actions.goto_home_dir,
+              ["w"] = fb_actions.goto_cwd,
+              ["t"] = fb_actions.change_cwd,
+              ["f"] = fb_actions.toggle_browser,
+              ["H"] = fb_actions.toggle_hidden,
+              ["s"] = fb_actions.toggle_all,
+            },
+          },
+        },
       },
       defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+          horizontal = {
+            prompt_position = "bottom",
+            preview_width = 0.5,
+          },
+          vertical = {
+            prompt_position = "bottom",
+            preview_height = 0.6,
+          },
+        },
         mappings = {
           i = {
             ["C-q"] = actions.close
@@ -46,6 +97,9 @@ return {
       }
     }
     require("telescope").load_extension("fzf")
+    require("telescope").load_extension("file_browser")
+
+    local fb_launch = require("telescope").extensions.file_browser.file_browser
 
     local opts = { noremap = true, silent = true }
 
@@ -68,6 +122,11 @@ return {
         }
       }
     end, opts)
+
+    vim.keymap.set("n", "<space>r", function()
+	    fb_launch()
+    end, opts)
+
 
     vim.keymap.set("n", "<Leader>d", builtin.find_files, opts)
     vim.keymap.set("n", "<Leader>f", builtin.live_grep, opts)
