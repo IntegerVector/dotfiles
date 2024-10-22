456cc5a68a60a1a3abffa389ff2b1f17a0c76bcf
IntegerVector <andrii.ruban@outlook.com>
2024-10-22 21:03:12 +0300

    shell: new prompt and text to ascii image binary

--- .shell/main.sh
+++ .shell/main.sh
@@ -2,7 +2,6 @@
 
 source ~/.shell/prompt.sh
 source ~/.shell/aliases.sh
-source ~/.shell/header-img.sh
 source ~/.shell/utils.sh
 source ~/.shell/commands.sh
 
@@ -21,5 +20,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-echo "$_HEADER_IMG_UWU"
+echo ""
+echo ""
+~/.shell/ttai " $USER "
 
