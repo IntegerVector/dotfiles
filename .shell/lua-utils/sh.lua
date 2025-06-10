ab46c6f4d344ac5f7521d786877bd51dbb358083
IntegerVector <andrii.ruban@outlook.com>
2025-06-10 17:48:55 +0300

    shell: formating fix

--- .shell/lua-utils/sh.lua
+++ .shell/lua-utils/sh.lua
@@ -1,109 +1,109 @@
 local M = {}
 
 local function arg(key, value)
-	if not value then
+  if not value then
     return key
   end
 
-	if type(value) == 'string' and #value > 0 then
+  if type(value) == 'string' and #value > 0 then
     return key..'=\''..value..'\''
   end
 
-	if type(value) == 'number' then
+  if type(value) == 'number' then
     return key..'='..tostring(value)
   end
 
-	if type(value) == 'boolean' and value == true then
+  if type(value) == 'boolean' and value == true then
     return key
   end
 
-	error('invalid argument type: ' .. type(value) .. ' = ' .. value)
+  error('invalid argument type: ' .. type(value) .. ' = ' .. value)
 end
 
 local function flatten(table)
-	local result = {args = {}, input = ''}
+  local result = {args = {}, input = ''}
 
-	local function f(t)
-		local keys = {}
+  local function f(t)
+    local keys = {}
 
-		for k = 1, #t do
-			keys[k] = true
-			local v = t[k]
+    for k = 1, #t do
+      keys[k] = true
+      local v = t[k]
 
-			if type(v) == 'table' then
-				f(v)
-			else
+      if type(v) == 'table' then
+        f(v)
+      else
         result.args[#result.args + 1] = v
-			end
-		end
+      end
+    end
 
-		for k, v in pairs(t) do
-			if k == '__input' then
-				result.input = result.input .. v
-			elseif not keys[k] and k:sub(1, 1) ~= '_' then
-				local key = '-'..k
+    for k, v in pairs(t) do
+      if k == '__input' then
+        result.input = result.input .. v
+      elseif not keys[k] and k:sub(1, 1) ~= '_' then
+        local key = '-'..k
 
-				if #k > 1 then
+        if #k > 1 then
           key = '-' ..key
         end
 
         result.args[#result.args + 1] = arg(key, v)
-			end
-		end
+      end
+    end
 
-	end
+  end
 
-	f(table)
+  f(table)
 
-	return result
+  return result
 end
 
 local function command(cmd, ...)
-	local prearg = {...}
+  local prearg = {...}
 
-	return function(...)
-		local args = flatten({...})
-		local s = cmd
+  return function(...)
+    local args = flatten({...})
+    local s = cmd
 
-		for _, v in ipairs(prearg) do
-			s = s .. ' ' .. v
-		end
+    for _, v in ipairs(prearg) do
+      s = s .. ' ' .. v
+    end
 
-		for _, v in pairs(args.args) do
-			s = s .. ' ' .. v
-		end
+    for _, v in pairs(args.args) do
+      s = s .. ' ' .. v
+    end
 
-		local p = io.popen(s, 'r')
+    local p = io.popen(s, 'r')
 
     if p ~= nil then
-		  local output = p:read('*a')
-		  local _, exit, status = p:close()
+      local output = p:read('*a')
+      local _, exit, status = p:close()
 
-		  local t = {
-			  __input = output,
-			  __exitcode = exit == 'exit' and status or 127,
-			  __signal = exit == 'signal' and status or 0,
-		  }
+      local t = {
+        __input = output,
+        __exitcode = exit == 'exit' and status or 127,
+        __signal = exit == 'signal' and status or 0,
+      }
 
-		  local mt = {
-		  	__index = function(self, k, ...)
-				  return _G[k]
-			  end,
-			  __tostring = function(self)
-				  return self.__input:match('^%s*(.-)%s*$')
-			  end
-		  }
+      local mt = {
+        __index = function(self, k, ...)
+          return _G[k]
+        end,
+        __tostring = function(self)
+          return self.__input:match('^%s*(.-)%s*$')
+        end
+      }
 
-		  return setmetatable(t, mt)
+      return setmetatable(t, mt)
     end
-	end
+  end
 end
 
 local mt = getmetatable(_G) or {}
 setmetatable(_G, mt)
 
 mt.__index = function(t, cmd)
-	return command(cmd)
+  return command(cmd)
 end
 
 M.sh = command
@@ -123,9 +123,9 @@ M['config:sh'] = function()
 end
 
 setmetatable(M, {
-	__call = function(_, cmd, ...)
-		return command(cmd, ...)
-	end
+  __call = function(_, cmd, ...)
+    return command(cmd, ...)
+  end
 })
 
 return M
