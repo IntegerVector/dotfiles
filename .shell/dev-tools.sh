9f281c472121959ce3b7ff48dcdec4c39d228e94
IntegerVector <andrii.ruban@outlook.com>
2026-02-07 23:05:05 +0200

    shell: add bun support

--- .shell/dev-tools.sh
+++ .shell/dev-tools.sh
@@ -5,3 +5,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+# bun completions
+[ -s "/Users/iv/.bun/_bun" ] && source "/Users/iv/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
