7abe42b9f9358dfe44bbb26ad50c13be6d293c4a
IntegerVector <andrii.ruban@outlook.com>
2026-02-12 16:23:49 +0200

    templates: add samba config template

--- link.sh
+++ link.sh
@@ -5,4 +5,5 @@ ln -s ~/Projects/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/Projects/dotfiles/.shell ~/.shell
 ln -s ~/Projects/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Projects/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Projects/dotfiles/.templates ~/.templates
 
