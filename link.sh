2e38a5eca90f6797ca027b8e3d674dbb1ee1b802
IntegerVector <andrii.ruban@outlook.com>
2026-04-16 21:40:37 +0300

    sh: update link.sh file

--- link.sh
+++ link.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-ln -s ~/Projects/dotfiles/.config/kitty ~/.config/kitty
 ln -s ~/Projects/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/Projects/dotfiles/.config/ghostty ~/.config/ghostty
+ln -s ~/Projects/dotfiles/.config/ghostty/config.ghostty '~/Library/Application Support/com.mitchellh.ghostty/config.ghostty'
 ln -s ~/Projects/dotfiles/.shell ~/.shell
 ln -s ~/Projects/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Projects/dotfiles/.vimrc ~/.vimrc
