5db3270df621c1d5b46ad23c28daef78dc13e1a8
IntegerVector <andrii.ruban@outlook.com>
2026-04-16 20:31:57 +0300

    sh: add git branch to prompt

--- .shell/prompt.sh
+++ .shell/prompt.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
 
-PS1="$(echo '\n\n\n %~') $(echo '\n\n   ')"
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+zstyle ':vcs_info:git:*' formats ' %b'
+zstyle ':vcs_info:git:*' actionformats ' %b|%a>'
+
+setopt PROMPT_SUBST
+PS1='%B%F{cyan}$(echo "\n\n\n %~")%f%b %F{yellow}${vcs_info_msg_0_}%f $(echo "\n\n   ")'
 
