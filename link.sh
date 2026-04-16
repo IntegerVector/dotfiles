51d3b57c2138ae39a26c799135a7f917b30f980b
IntegerVector <andrii.ruban@outlook.com>
2026-04-16 22:37:04 +0300

    sh: update link.sh

--- link.sh
+++ link.sh
@@ -2,9 +2,14 @@
 
 ln -s ~/Projects/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/Projects/dotfiles/.config/ghostty ~/.config/ghostty
-ln -s ~/Projects/dotfiles/.config/ghostty/config.ghostty '~/Library/Application Support/com.mitchellh.ghostty/config.ghostty'
 ln -s ~/Projects/dotfiles/.shell ~/.shell
 ln -s ~/Projects/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Projects/dotfiles/.vimrc ~/.vimrc
 ln -s ~/Projects/dotfiles/.templates ~/.templates
+
+case $(uname -s) in
+  "Darwin")
+    ln -s ~/Projects/dotfiles/.config/ghostty/config.ghostty '~/Library/Application Support/com.mitchellh.ghostty/config.ghostty'
+    ;;
+esac
 
